28b47cc73598b75390e533eb5d4373a5ed1f850e
miiles08 <miles.hudock28@gmail.com>
2025-12-01 10:05:24 -0500

    Added interactable litter box, can now pour litter

---
+++ Assets/LitterInteraction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitterPour : MonoBehaviour
+{
+    public GameObject litter;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("BoxOfLitter"))
+        {
+            if (!litter.activeSelf)
+            {
+                litter.SetActive(true);
+            }
+        }
+    }
+}
