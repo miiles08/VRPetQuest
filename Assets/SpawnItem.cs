fae3cfabd5a20c664dad2f00edea36b88c3931a8
miiles08 <miles.hudock28@gmail.com>
2025-11-30 21:25:05 -0500

    Implimented functioning UI for purchases. Stat screen started and updated money spent. Needs happiness update and more features

--- Assets/SpawnItem.cs
+++ Assets/SpawnItem.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.EventSystems; // Needed for IPointerClickHandler
+using TMPro;
 
 public class SpawnBoxVR : MonoBehaviour
-{
+{   
+    public TMP_Text totalText;   // Assign the UI text that shows total
     public GameObject boxPrefab;   // Drag your prefab here
     public Transform spawnPoint;   // Drag the GameObject where items should appear
 
@@ -12,6 +14,19 @@ public class SpawnBoxVR : MonoBehaviour
         if (boxPrefab != null && spawnPoint != null)
         {
             Instantiate(boxPrefab, spawnPoint.position, spawnPoint.rotation);
+
         }
     }
+
+    public void BuyItem(float price)
+    {
+        GlobalTotals.totalSpent += price;              // Add price to total
+        UpdateTotalText();                 // Update the UI
+    }
+
+    private void UpdateTotalText()
+    {
+        totalText.text = "Species: Cat\nHappiness:\n$$$ Spent:" + GlobalTotals.totalSpent.ToString("0.00");
+    }
 }
+
